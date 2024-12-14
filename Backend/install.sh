9959d9929c13b3ba0751815f060f1fa3bed7d0bf
Hugo Sanz González <80487270+hsanzg@users.noreply.github.com>
2024-12-14 10:48:45 +0100

    add dep

--- Backend/install.sh
+++ Backend/install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 pip3 install poetry
+rm -f poetry.lock
 poetry install
+
--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from typing import Annotated
+from fastapi import FastAPI, Form
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+@app.post('/guardar_paciente')
+def guardar_paciente(id: Annotated[str, Form()],
+                     nombre: Annotated[str, Form()]):
+  return {'id': id, 'nombre': nombre}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
