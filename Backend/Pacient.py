dc1bd2867789fe8c9c72b176869222f53d5df98f
Lucas Bazilio <45127111+lucasebazilio@users.noreply.github.com>
2024-12-14 12:23:14 +0100

    Dado un Paciente, añadimos sintomas graves, añadimos síntomas generales

--- Backend/Pacient.py
+++ Backend/Pacient.py
@@ -44,3 +44,43 @@ class Pacient:
         "Contusió pulmonar",
         "Exacerbació aguda (ExA) de la malaltia pulmonar intersticial de base (com en la FPI)"]
     )
+
+
+    def añadir_sintomas_graves(self, sintomas_graves: List[str]):
+        """
+        Añade una lista de síntomas graves al atributo 'síntomas' del paciente.
+        """
+        sintomas_a_agregar = [
+            "Febre",
+            "Desaturació",
+            "Increment respiracions per minut o respiracions per minut > 19",
+            "Tiratge muscular per respirar",
+            "Ofeg en repòs",
+            "Auscultació de xiulets (sibilants) o altres sorolls diferents als que té el pacient de base",
+            "To blau distal als dits o llavis"
+        ]
+        for sintoma in sintomas_graves:
+            if sintoma in sintomas_a_agregar and sintoma not in self.síntomas:
+                self.síntomas.append(sintoma)
+
+    def añadir_sintomas_generales(self, sintomas_generales: List[str]):
+        """
+        Añade una lista de síntomas generales al atributo 'síntomas' del paciente.
+        """
+        sintomas_a_agregar = [
+            "Increment o aparició d’ofeg i/o tos en els darrers dies (maxim darreres 2 setmanes), sense altres símptomes o amb altres símptomes",
+            "Increment mucositat i congestió nasal",
+            "Increment mucositat i dolor gola",
+            "Increment mucositat i febre",
+            "Dolor toràcic",
+            "Xiulets",
+            "Increment de la mucositat, mal estat general, +/- congestió nasal o dolor gola",
+            "Dolor toràcic e. increment d’ofec sense tos ni altres símptomes"
+        ]
+        for sintoma in sintomas_generales:
+            if sintoma in sintomas_a_agregar and sintoma not in self.síntomas:
+                self.síntomas.append(sintoma)
+
+
+
+
