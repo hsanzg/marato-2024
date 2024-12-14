56a44d0553a4fd0c251d0d8a5a44ced498574170
Hugo Sanz González <80487270+hsanzg@users.noreply.github.com>
2024-12-14 12:09:50 +0100

    Cambios

--- Backend/main.py
+++ Backend/main.py
@@ -1,35 +1,68 @@
 from typing import Annotated
-from fastapi import FastAPI, Form
+from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import RedirectResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
+
+# Tipos.
+
+class Paciente:
+  def __init__(self):
+    pass
 
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
+
+# Páginas comunes.
+
+@app.get('/', response_class=HTMLResponse)
+def index(req: Request):
+  return templates.TemplateResponse(
+    name='index.html', context={'request': req}
+  )
+
+@app.get('/leerqr', response_class=HTMLResponse)
+def paciente(req: Request):
+  return templates.TemplateResponse(
+    name='leerqr.html', context={'request': req}
+  )
+
+@app.get('/verqr', response_class=HTMLResponse)
+def paciente(req: Request):
+  return templates.TemplateResponse(
+    name='verqr.html', context={'request': req}
+  )
+
+@app.get('/datos_paciente/{id}')
+def datos_paciente(id: int):
+  print(f'cargando datos de paciente {id}')
+  return {'id': id}
+
+app.mount('/static', StaticFiles(directory='static', html=True), name='static')
+
+# Páginas paciente.
+
+@app.get('/paciente/{id}', response_class=HTMLResponse)
+def paciente(req: Request, id: str):
+  pac = Paciente()
+  return templates.TemplateResponse(
+    name='paciente.html',
+    context={'request': req, 'pac': pac}
+  )
 
 @app.post('/guardar_paciente')
 def guardar_paciente(id: Annotated[str, Form()],
                      nombre: Annotated[str, Form()]):
   print('guardando datos de paciente')
   print('redirigiendo a QR de paciente')
-  #return {'id': id, 'nombre': nombre}
-  return RedirectResponse(f'verqr.html?id={id}')
+  return RedirectResponse(f'verqr?id={id}', status_code=303) # POST->GET
 
-@app.get('/datos_especialista/{id_paciente}')
-def datos_especialista(id_paciente: int):
-  print(f'cargando datos de paciente {id_paciente} para especialista')
-  return {'id': id_paciente}
+# Páginas especialista.
 
-@app.get('/datos_urgencia/{id_paciente}')
-def datos_urgencia(id_paciente: int):
-  print(f'cargando datos de paciente {id_paciente} para urgencia')
-  return {'id': id_paciente}
-
-@app.get('/datos_paciente/{id}')
-def datos_paciente(id: int):
-  print(f'cargando datos de paciente {id} para paciente')
-  return {'id': id}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "query": q}
-
-app.mount('/', StaticFiles(directory='static', html=True), name='static')
+@app.get('/especialista/{pac_id}', response_class=HTMLResponse)
+def paciente(req: Request, pac_id: int):
+  pac = Paciente()
+  return templates.TemplateResponse(
+    name='especialista.html',
+    context={'request': req, 'pac': pac}
+  )
