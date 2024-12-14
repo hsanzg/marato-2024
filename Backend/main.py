9eae09d40e4bb0e3771abbd0aa21a8550c98da19
Hugo Sanz González <80487270+hsanzg@users.noreply.github.com>
2024-12-14 10:55:05 +0100

    redirect

--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,18 @@
 from typing import Annotated
 from fastapi import FastAPI, Form
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
 @app.post('/guardar_paciente')
 def guardar_paciente(id: Annotated[str, Form()],
                      nombre: Annotated[str, Form()]):
-  return {'id': id, 'nombre': nombre}
+  print('guardando datos de paciente')
+  print('generando valor QR de paciente')
+  #return {'id': id, 'nombre': nombre}
+  return RedirectResponse('verqr.html')
+
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
