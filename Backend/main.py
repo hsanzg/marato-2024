d9fcc5ee2117433e6af3e45721727c3d23fd0dd8
Hugo Sanz González <80487270+hsanzg@users.noreply.github.com>
2024-12-14 11:26:55 +0100

    cargar datos

--- Backend/main.py
+++ Backend/main.py
@@ -13,15 +13,20 @@ def guardar_paciente(id: Annotated[str, Form()],
   #return {'id': id, 'nombre': nombre}
   return RedirectResponse(f'verqr.html?id={id}')
 
-@app.get('/datos_especialista.html')
-def datos_especialista(id_paciente: Annotated[str, Form()]):
+@app.get('/datos_especialista/{id_paciente}')
+def datos_especialista(id_paciente: int):
   print(f'cargando datos de paciente {id_paciente} para especialista')
   return {'id': id_paciente}
 
-@app.get('/datos_urgencia.html')
-def datos_urgencia(id_paciente: Annotated[str, Form()]):
+@app.get('/datos_urgencia/{id_paciente}')
+def datos_urgencia(id_paciente: int):
   print(f'cargando datos de paciente {id_paciente} para urgencia')
   return {'id': id_paciente}
+
+@app.get('/datos_paciente/{id}')
+def datos_paciente(id: int):
+  print(f'cargando datos de paciente {id} para paciente')
+  return {'id': id}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
